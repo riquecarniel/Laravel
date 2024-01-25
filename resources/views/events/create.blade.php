e0cd9312c4375d6735a9f52289e85b97a18c72ac
riquecarniel <henriquecarniel@outlook.com>
2024-01-25 15:33:49 -0300

    checkbox

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -31,6 +31,33 @@
             <label for="title">Descrição do evento:</label>
             <textarea name="description" id="description" class="form-control" placeholder="O que vai acontecer no evento"></textarea>
         </div>
+        <div class="form-group">
+            <label for="title">Adicione itens de infraestrutura:</label>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Cadeiras"> Cadeiras
+            </div>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Palco"> Palco
+            </div>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Projetor"> Projetor
+            </div>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Microfone"> Microfone
+            </div>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Open Food"> Open Food
+            </div>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Open Beer"> Open Beer
+            </div>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Café"> Café
+            </div>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Água"> Água
+            </div>
+        </div>
         <input type="submit" class="btn btn-primary" value="Criar Evento">
     </form>
 </div>
