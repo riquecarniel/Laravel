f6ac26e0fdfd728807e8b2b624003db94a3214aa
riquecarniel <henriquecarniel@outlook.com>
2024-01-26 14:59:49 -0300

    modificação

--- app/Http/Controllers/Eventcontroller.php
+++ app/Http/Controllers/Eventcontroller.php
@@ -25,9 +25,11 @@ class EventController extends Controller
         $event = new Event;
 
         $event->title = $request->title;
+        $event->date = $request->date;
         $event->city = $request->city;
         $event->private = $request->private;
         $event->description = $request->description;
+        $event->items = $request->items;
 
         // image upload
 
--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -12,9 +12,16 @@
                 <div id="info-container" class="col-md-6">
                     <h1>{{ $event->title }}</h1>
                     <p class="event-city"><ion-icon name="location-outline"></ion-icon> {{ $event->city }} </p>
+                    <p class="event-date"><ion-icon name="calendar-number-outline"></ion-icon> {{ $event->date }} </p>
                     <p class="events-participants"><ion-icon name="people-outline"></ion-icon> x participantes </p>
                     <p class="event-owner"><ion-icon name="star-outline"></ion-icon> Dono do Evento </p>
                     <a href="#" class="btn btn-primary" id="event-submit">Confirmar Presença</a>
+                    <h3>O evento conta com:</h3>
+                    <ul id="items-list">
+                        @foreach ($event->items as $item)
+                            <li><ion-icon name="play-outline"></ion-icon> <span>{{ $item }} </span></li>
+                        @endforeach
+                    </ul>
                 </div>
                 <div class="col-md-12" id="description-container">
                     <h3>Sobre o evento:</h3>
