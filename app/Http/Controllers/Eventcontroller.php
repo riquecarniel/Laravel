01cb9a06769b5837174a1367ea7505314326d518
riquecarniel <henriquecarniel@outlook.com>
2024-01-29 11:34:36 -0300

    inserção bancos

--- app/Http/Controllers/Eventcontroller.php
+++ app/Http/Controllers/Eventcontroller.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Event;
+use App\Models\User;
 
 class EventController extends Controller
 {
@@ -57,6 +58,9 @@ class EventController extends Controller
 
         }
 
+        $user = auth()->user();
+        $event->user_id = $user->id;
+
         $event->save();
 
         return redirect('/')->with('msg', 'Evento criado com sucesso!');
@@ -67,7 +71,9 @@ class EventController extends Controller
 
         $event = Event::findOrFail($id);
 
-        return view('events.show', ['event' => $event]);
+        $eventOwner = User::where('id', $event->user_id)->first()->toArray();
+
+        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner]);
         
     }
 
