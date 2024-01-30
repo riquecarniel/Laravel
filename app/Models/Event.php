374984957879e2644e752a0fee5258de4ee091f4
riquecarniel <henriquecarniel@outlook.com>
2024-01-30 10:57:06 -0300

    Aula 27

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -14,6 +14,8 @@ class Event extends Model
     ];
 
     protected $dates = ['date'];
+
+    protected $guarded = [];
     
     public function user() {
         return $this->belongsTo('App\Models\User');
--- resources/views/events/dashboard.blade.php
+++ resources/views/events/dashboard.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.main')
 
-@section('title', 'Dashboard')
+@section('title', 'Dasboard')
 
 @section('content')
 
@@ -8,7 +8,7 @@
     <h1>Meus Eventos</h1>
 </div>
 <div class="col-md-10 offset-md-1 dashboard-events-container">
-    @if (count($events) > 0)
+    @if(count($events) > 0)
     <table class="table">
         <thead>
             <tr>
@@ -19,18 +19,25 @@
             </tr>
         </thead>
         <tbody>
-            @foreach ($events as $event)
+            @foreach($events as $event)
                 <tr>
-                    <td scope="row">{{ $loop->index + 1 }}</td>
+                    <td scropt="row">{{ $loop->index + 1 }}</td>
                     <td><a href="/events/{{ $event->id }}">{{ $event->title }}</a></td>
                     <td>0</td>
-                    <td><a href="#">Editar</a> <a href="#">Deletar</a></td>
+                    <td>
+                        <a href="events/edit/{{ $event->id }}" class="btn btn-info edit-btn"><ion-icon name="create-outline"></ion-icon> Editar</a> 
+                        <form action="/events/{{ $event->id }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger delete-btn"><ion-icon name="trash-outline"></ion-icon> Deletar</button>
+                        </form>
+                    </td>
                 </tr>
-            @endforeach
+            @endforeach    
         </tbody>
     </table>
     @else
-          <p>Você ainda não tem eventos <a href="/events/create">Criar Evento</a></p>  
+    <p>Você ainda não tem eventos, <a href="/events/create">criar evento</a></p>
     @endif
 </div>
 
