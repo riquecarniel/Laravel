abcb5d36da250b92ea62b54c8ad06412bc442891
riquecarniel <henriquecarniel@outlook.com>
2024-01-30 15:26:40 -0300

    Aula 30

--- routes/web.php
+++ routes/web.php
@@ -23,10 +23,10 @@ Route::delete('/events/{id}', [EventController::class, 'destroy'])->middleware('
 Route::get('/events/edit/{id}', [EventController::class, 'edit'])->middleware('auth');
 Route::put('/events/update/{id}', [EventController::class, 'update'])->middleware('auth');
 
-Route::get('/contato', function () {
-    return view('Contato');
-});
+Route::get('/contato', function () {return view('Contato');});
 
 Route::get('/dashboard', [EventController::class, 'dashboard'])->middleware('auth');
 
 Route::post('/events/join/{id}', [EventController::class, 'joinEvent'])->middleware('auth');
+
+Route::delete('/events/leave/{id}', [EventController::class, 'leaveEvent'])->middleware('auth');
